028ca7ec6654cd206f33e642dcd6057e427712db
kay <kousikac06@gmail.com>
2023-01-09 14:48:37 +0800

    remove not use class

--- nginx/src/app/Http/Controllers/Api/StrategyController.php
+++ nginx/src/app/Http/Controllers/Api/StrategyController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Pattern\Strategy\AmericaIceCream;
-use App\Pattern\Strategy\ButterfatStrategy;
 use App\Services\StrategyService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
